5f1a544466d2bc86d261b507ec099f9e4c5dbe66
KyojunYoon <hhyon79@naver.com>
2021-03-12 16:36:07 +0900

    first upload

---
+++ Assets/NewBehaviourScript1.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player
+{
+    private string name;
+    private int level = 1;
+        
+    public Player(string _name)
+    {
+        this.name = _name;
+    }
+    public void GetInfo()
+    {
+        Debug.Log("이름 :" + this.name);
+        Debug.Log("레벨 :" + this.level);
+    }
+    public void LevelUp()
+    {
+        this.level++;
+        Debug.Log("레벨업!");
+    }
+}
+public class NewBehaviourScript1 : MonoBehaviour
+{
+    void Start()
+    {
+        Player myPlayer = new Player("윤교준");
+       // myPlayer.name = "윤교준";
+        myPlayer.GetInfo();
+        myPlayer.LevelUp();
+        myPlayer.GetInfo();
+    }
+}
